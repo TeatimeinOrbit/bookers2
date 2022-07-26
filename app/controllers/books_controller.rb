c3fd0edc2e4cad4c5a73923c11efd94f7589187c
TeatimeinOrbit <108617356+TeatimeinOrbit@users.noreplay.github.com>
2022-07-26 16:22:49 +0000

    [Update]4-25 bookのエディットページまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,9 +14,26 @@ class BooksController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @user = current_user
+    @book = Book.new
+    @users = User.all
   end
 
   def show
+    @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
